c8df8db24f61b96b38e6e3aadb75917d20be6692
C342 <mbuzurin2024@elam.co.uk>
2025-10-22 10:40:21 +0100

    healthbar in prog

--- Riding Shotgun/Assets/Scripts/PlayerHealth.cs
+++ Riding Shotgun/Assets/Scripts/PlayerHealth.cs
@@ -1,8 +1,13 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class PlayerHealth : MonoBehaviour
+public class Health : MonoBehaviour
 {
     [SerializeField] private float maxHealth = 100f;
+
+    [SerializeField] private float maxDamageTaken = 15f;
+    [SerializeField] private float minDamageTaken = 2.5f;
+
     private float currentHealth;
 
     private void Start()
@@ -10,10 +15,29 @@ public class PlayerHealth : MonoBehaviour
         currentHealth = maxHealth;
     }
 
-    void ChangeHealth(float amount)
+    private void OnCollisionEnter(Collision collision)
+    {
+        Health playerHealth = collision.gameObject.GetComponent<Health>();
+
+        if (playerHealth != null)
+        {
+            float randomDamage = Random.Range(minDamageTaken, maxDamageTaken);
+
+            playerHealth.ChangeHealth(-randomDamage);
+
+            Debug.Log($"{collision.gameObject.name} took {randomDamage:F1} damage!");
+        }
+    }
+
+    public void ChangeHealth(float amount)
     {
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log($"{gameObject.name} Health: {currentHealth}/{maxHealth}");
 
-        Debug.Log(currentHealth + "/" + maxHealth);
+        if (currentHealth <= 0)
+        {
+            Scene currentScene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(currentScene.name);
+        }
     }
 }
